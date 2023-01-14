586c714eb4d91eff55647bbc9b910207fc38bc47
NikosKarapiperis <92748811+NikosKarapiperis@users.noreply.github.com>
2023-01-14 17:54:53 +0200

    Delete PaymentRepository.java

--- springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.springbootdemo.repository;
-
-import gr.hua.dit.springbootdemo.entities.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Integer> {
-}
