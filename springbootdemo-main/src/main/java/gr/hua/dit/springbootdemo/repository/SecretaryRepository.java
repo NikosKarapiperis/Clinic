378948483d79b3de54da5dc5e48e61fde1fd8b51
NikosKarapiperis <92748811+NikosKarapiperis@users.noreply.github.com>
2023-01-20 20:30:09 +0200

    Update SecretaryRepository.java

--- springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/repository/SecretaryRepository.java
+++ springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/repository/SecretaryRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface SecretaryRepository extends JpaRepository<Secretary,Integer> {
 
-    //Optional<Secretary> delete(int id);
+    Secretary findById(int id);
 }
