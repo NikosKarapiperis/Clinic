1e9a62fa192fe3e848679ef730ac15ecfdb7c4dd
NikosKarapiperis <92748811+NikosKarapiperis@users.noreply.github.com>
2023-01-20 23:37:31 +0200

    Update SecretaryController.java

--- springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/controller/SecretaryController.java
+++ springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/controller/SecretaryController.java
@@ -8,8 +8,11 @@ import gr.hua.dit.springbootdemo.entities.Patient;
 import gr.hua.dit.springbootdemo.entities.Secretary;
 import gr.hua.dit.springbootdemo.repository.SecretaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
