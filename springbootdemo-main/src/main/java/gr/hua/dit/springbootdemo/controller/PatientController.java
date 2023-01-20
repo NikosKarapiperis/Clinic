eac869c4aead001ba75e5907c5db27c22122f55f
NikosKarapiperis <92748811+NikosKarapiperis@users.noreply.github.com>
2023-01-20 23:08:48 +0200

    Update PatientController.java

--- springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/controller/PatientController.java
+++ springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/controller/PatientController.java
@@ -15,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
