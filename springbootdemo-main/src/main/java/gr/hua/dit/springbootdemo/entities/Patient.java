4c6ea99f10ee866a29e0f979abc86ac870d15be5
NikosKarapiperis <92748811+NikosKarapiperis@users.noreply.github.com>
2023-02-08 17:04:28 +0200

    Update Patient.java

--- springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/entities/Patient.java
+++ springbootdemo-main/src/main/java/gr/hua/dit/springbootdemo/entities/Patient.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Table(name = "patient")
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
     private int id;
 
